811efd0d3422954fe282671623c9be6a668cc815
Nestor Zamili <nestorzamili@gmail.com>
2026-09-08 06:05:53 +0700

    fix: drop unused IconBrandDiscord import after merge (support nav removed)

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -3,7 +3,6 @@ import { router, usePage } from '@inertiajs/vue3';
 import {
     IconAffiliate,
     IconAlertTriangle,
-    IconBrandDiscord,
     IconCalendar,
     IconChartBar,
     IconChevronRight,
